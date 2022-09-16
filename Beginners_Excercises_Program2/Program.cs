96a2674fd9defa872325fbfd78c0dc0e888aba72
Rishika Arya <meghaar474@gmail.com>
2022-09-15 23:14:29 -0700

    Program 2

---
+++ Beginners_Excercises_Program2/Program.cs
@@ -0,0 +1,28 @@
+﻿//2.Write a program which takes two numbers from the console and displays the maximum of the two.
+
+using System;
+namespace Beginners_Exercise
+{
+    class Program2
+    {
+        public static void Main(string[] args)
+        {
+            var maximum = 0;
+            Console.WriteLine("Enter the first number : ");
+            var number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number : ");
+            var number2 = Convert.ToInt32(Console.ReadLine());
+
+            if(number1 > number2)
+            {
+                maximum = number1;
+            }
+            else
+            {
+                 maximum = number2;
+            }
+            Console.WriteLine("The maximum of Two Numbers Are : " +maximum);
+        }
+    }
+}
